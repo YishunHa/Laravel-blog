03090ec7ebb17d1055a8f940aa28efe444f3969b
YishunHa <u1650778@unimail.hud.ac.uk>
2018-01-23 20:37:46 +0000

    fix navbar

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -25,9 +25,10 @@
   <ul class="nav navbar-nav navbar-right">
         <!-- Authentication Links -->
         @if (Auth::guest())
-            <li><a href="{{ url('/login') }}">Login</a></li>
-            <li><a href="{{ url('/register') }}">Register</a></li>
+            <li><a href="{{ url('/login') }}">Controll-Pannel</a></li>
+
         @else
+
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -50,6 +51,7 @@
                     </li>
                 </ul>
             </li>
+            <li><a href="{{ url('/register') }}">Register</a></li>
         @endif
 
   </ul>
